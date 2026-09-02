36981d5c171daecf1af1641f23bac82f182f3a7d
ganeshyarramsetti22 <ganeshyarramsetti2244.com>
2026-09-02 10:00:35 +0530

    ui and api change

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Hello from full stack hari API .....api version",
+        message: "Hello from full stack hari API .....api version2",
         version: "2.0.0"
     });
 });
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Full Stack CI/CD Demo---version3</h1>
+      <h1>Full Stack CI/CD Demo---version4</h1>
 
       <h2>UI Application</h2>
 
